0b041f57a50c20d0273964fac07fac588645e05b
jerin <jerin.antony@ignitarium.com>
2020-02-21 10:47:04 +0530

    working code

---
+++ mnist_gen.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+import os
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+import math
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+from keras.utils import Sequence
+
+class DataGenerator(Sequence): #tf.compat.v2.keras.utils.Sequence
+ 
+    def __init__(self, X_data , y_data, batch_size, dim, n_classes,
+                 to_fit, shuffle = True):        
+        self.batch_size = batch_size
+        self.X_data = X_data
+        self.labels = y_data
+        self.y_data = y_data
+        self.to_fit = to_fit
+        self.n_classes = n_classes
+        self.dim = dim
+        self.shuffle = shuffle
+        self.n = 0
+        self.list_IDs = np.arange(len(self.X_data))
+        self.on_epoch_end()    
+    def __next__(self):
+        # Get one batch of data
+        data = self.__getitem__(self.n)
+        # Batch index
+        self.n += 1
+        
+        # If we have processed the entire dataset then
+        if self.n >= self.__len__():
+            self.on_epoch_end
+            self.n = 0
+        
+        return data    
+    def __len__(self):
+        # Return the number of batches of the dataset
+        return math.ceil(len(self.indexes)/self.batch_size)    
+    def __getitem__(self, index):
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:
+            (index+1)*self.batch_size]        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        
+        X = self._generate_x(list_IDs_temp)
+        
+        if self.to_fit:
+            y = self._generate_y(list_IDs_temp)
+            return X, y
+        else:
+            return X   
+    def on_epoch_end(self):
+        
+        self.indexes = np.arange(len(self.X_data))
+        
+        if self.shuffle: 
+            np.random.shuffle(self.indexes)    
+    def _generate_x(self, list_IDs_temp):
+               
+        X = np.empty((self.batch_size, *self.dim))
+        
+        for i, ID in enumerate(list_IDs_temp):
+            
+            X[i,] = self.X_data[ID]
+            
+            # Normalize data
+        X = (X/255.0).astype('float32')
+            
+        return X[:,:,:, np.newaxis]    
+    def _generate_y(self, list_IDs_temp):
+        
+        y = np.empty(self.batch_size)
+        
+        for i, ID in enumerate(list_IDs_temp):
+            
+            y[i] = self.y_data[ID]
+            
+        return keras.utils.to_categorical(
+                y,num_classes=self.n_classes)
+                
+n_classes = 10
+input_shape = (28, 28)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(28, 28 , 1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(n_classes, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+              
+              
+train_generator = DataGenerator(x_train, y_train, batch_size = 64,
+                                dim = input_shape,
+                                n_classes=10, 
+                                to_fit=True, shuffle=True)
+val_generator =  DataGenerator(x_test, y_test, batch_size=64, 
+                               dim = input_shape, 
+                               n_classes= n_classes, 
+                               to_fit=True, shuffle=True)
+                               
+images, labels = next(train_generator)
+print(images.shape)
+print(labels.shape)
+
+steps_per_epoch = len(train_generator)
+validation_steps = len(val_generator)
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=steps_per_epoch,
+        epochs=10,
+        validation_data=val_generator,
+        validation_steps=validation_steps)
+        
+        
+                   
+                                          
+              
+              
+              
+              
+              
+              
+              
+              
+                
